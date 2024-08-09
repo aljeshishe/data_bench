252f9a6a1bd9f8b509569ae8776a61df621b3fdd
Aleksei Grachev <aleksei.grachev@alberblanc.com>
2024-08-09 11:55:57 +0000

    added click

--- test_ray_data.py
+++ test_ray_data.py
@@ -1,4 +1,5 @@
 import time
+import click
 import ray
 import ray.data
 import torch
@@ -11,39 +12,42 @@ import dotenv
 
 
 dotenv.load_dotenv()
+@click.command()
+@click.option("-c", "--create_dataset", is_flag=True)
+def main(create_dataset):
+    # params
+    s3_uri = "s3://ab-users/grachev/ray_benchmark/20gb.parquet"
+    mvalues = 3000 # 20 GB
+    part_mvalues = 8
+    cols = 100
 
-# params
-s3_uri = "s3://ab-users/grachev/ray_benchmark/20gb.parquet"
-mvalues = 3000 # 20 GB
-part_mvalues = 8
-cols = 100
+    if create_dataset:
+        # create dataset in s3
+        utils.remove(s3_uri)
+        utils.write_dummy_dataset(s3_uri, mvalues=mvalues, part_mvalues=part_mvalues, cols=cols)
 
-# create dataset in s3
-utils.remove(s3_uri)
-utils.write_dummy_dataset(s3_uri, mvalues=mvalues, part_mvalues=part_mvalues, cols=cols)
+    ray.data.set_progress_bars(False)
+    ray.init(logging_level="INFO")
+    ds = ray.data.read_parquet(s3_uri)
+    logger.info(f"Dataset: rows={ds.count()} cols={len(ds.columns())}")
 
-ray.data.set_progress_bars(False)
-ray.init(logging_level="INFO")
-ds = ray.data.read_parquet(s3_uri)
-logger.info(f"Dataset: rows={ds.count()} cols={len(ds.columns())}")
-
-# Define batch size and shuffle the dataset
-batch_size = 1024 * 1024
-# dataset = dataset.random_shuffle()  # Shuffle the entire dataset
-# row_count = dataset.count()
-# Use iter_batches to iterate over batches directly
-total_start_ts = time.time()
-with tqdm(total=ds.count() // batch_size) as pbar:
-    start_ts = time.time()
-    train_dataloader = ds.iter_torch_batches(batch_size=batch_size, dtypes=torch.float32)
-    for batch in train_dataloader:
-        mvalues_per_sec = cols * batch_size / (time.time() - start_ts) / M
+    # Define batch size and shuffle the dataset
+    batch_size = 1024 * 1024
+    # dataset = dataset.random_shuffle()  # Shuffle the entire dataset
+    # row_count = dataset.count()
+    # Use iter_batches to iterate over batches directly
+    total_start_ts = time.time()
+    with tqdm(total=ds.count() // batch_size) as pbar:
         start_ts = time.time()
-        pbar.set_postfix_str(f"mvalues/s={mvalues_per_sec:.2f}")
-        pbar.update()
-        # X = batch["X"]
-        # columns = [col for col in X.columns if col != "COIN"]
-        # tensors = {col: torch.tensor(X[columns].values, dtype=torch.float32) for col in columns}
+        train_dataloader = ds.iter_torch_batches(batch_size=batch_size, dtypes=torch.float32)
+        for batch in train_dataloader:
+            mvalues_per_sec = cols * batch_size / (time.time() - start_ts) / M
+            start_ts = time.time()
+            pbar.set_postfix_str(f"mvalues/s={mvalues_per_sec:.2f}")
+            pbar.update()
 
-total_mvalues_per_sec = cols * ds.count() / M / (time.time() - total_start_ts)
-logger.info(f"Total mvalues/s={total_mvalues_per_sec:.2f}")
+    total_mvalues_per_sec = cols * ds.count() / M / (time.time() - total_start_ts)
+    logger.info(f"Total mvalues/s={total_mvalues_per_sec:.2f}")
+
+if __name__ == "__main__":
+    main()
